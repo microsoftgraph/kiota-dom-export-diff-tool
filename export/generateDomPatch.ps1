1a1c1d0cb31bef4bbcc8342513a09ec6e091ef2c
Vincent Biret <vibiret@microsoft.com>
2024-08-02 14:57:40 -0400

    feat: adds diff script
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ export/generateDomPatch.ps1
@@ -0,0 +1,32 @@
+param (
+	[string]
+	$fileNameToDiff = "kiota-dom-export.txt",
+    [string]
+    $initialCommitSha = "",
+    [string]
+    $finalCommitSha = ""
+)
+if ($null -eq $fileNameToDiff -or "" -eq $fileNameToDiff) {
+    $fileNameToDiff = "kiota-dom-export.txt"
+}
+$relativePath = Get-ChildItem -Recurse -Filter $fileNameToDiff | Select-Object -First 1
+if ($null -eq $relativePath) {
+    Write-Warning "No file found with the name $fileNameToDiff"
+} else {
+    if ($initialCommitSha -eq "" -or $finalCommitSha -eq "") {
+        $result = git format-patch -1 HEAD --minimal -U0 -w $relativePath
+    } else {
+        $result = git format-patch $initialCommitSha..$finalCommitSha --minimal -U0 -w $relativePath
+    }
+
+    if ((Test-Path $result) -and (Test-Path Env:GITHUB_OUTPUT)) {
+        write-host "Patch file generated at $result"
+        "isFilePresent=true" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+        "patchFilePath=$result" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+        exit 0
+    }
+}
+if (Test-Path Env:GITHUB_OUTPUT) {
+    write-host "No patch file generated"
+    "isFilePresent=false" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+}
